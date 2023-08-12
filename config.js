ede9837efcd45ee7abb769b4d4d93852de2f68a0
JubinKamdar-NU <113157630+JubinKamdar-NU@users.noreply.github.com>
2023-08-12 11:52:48 -0400

    Update config.js
    Firebase Update

--- config.js
+++ config.js
@@ -7,13 +7,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC2JC72bCTk3k8t1aa81J9JIsd4g0mMk4c",
-  authDomain: "diallapp-574cb.firebaseapp.com",
-  databaseURL: "https://diallapp-574cb-default-rtdb.firebaseio.com",
-  projectId: "diallapp-574cb",
-  storageBucket: "diallapp-574cb.appspot.com",
-  messagingSenderId: "555709554780",
-  appId: "1:555709554780:web:d48b1cf6f1504574110699"
+ Your Firebase Config
 };
 
 // let firebaseApp;
